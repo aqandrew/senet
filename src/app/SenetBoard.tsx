8f3be388d40cc3ec984108be1f939a6bdfbad4aa
Andrew Aquino <dawneraq@gmail.com>
2024-07-05 13:02:25 -0700

    feat: reverse second row's column order, using flex \ grid

--- src/app/SenetBoard.tsx
+++ src/app/SenetBoard.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import { useState } from 'react';
+import clsx from 'clsx';
+
+const NUM_COLUMNS = 10;
+const NUM_ROWS = 3;
 
 const BLACK_PAWN = '♟';
 const WHITE_PAWN = '♙';
-type Space = typeof BLACK_PAWN | typeof WHITE_PAWN | null;
+type Item = typeof BLACK_PAWN | typeof WHITE_PAWN | null;
 
 const HOUSE_OF_REBIRTH = 14;
 const SAFE_HOUSE_1 = 25;
@@ -13,28 +17,47 @@ const SAFE_HOUSE_2 = 27;
 const SAFE_HOUSE_3 = 28;
 
 export default function SenetBoard() {
-	const [spaces, setSpaces] = useState<Space[]>(
+	const [spaces, setSpaces] = useState<Item[]>(
 		new Array(30)
 			.fill(null)
 			.map((_, i) => (i < 10 ? (i % 2 ? BLACK_PAWN : WHITE_PAWN) : null))
 	);
 
 	return (
-		// TODO reverse second row's column order
-		<div className="grid grid-cols-10 grid-rows-3 gap-2">
-			{spaces.map((space, i) => (
-				<div
-					className="aspect-square grid place-items-center border-2 text-5xl cursor-pointer select-none"
-					key={i}
-				>
-					{i === HOUSE_OF_REBIRTH ? '☥' : null}
-					{i === SAFE_HOUSE_1 ? '𓆾' : null}
-					{i === HOUSE_OF_WATER ? '𓈗' : null}
-					{i === SAFE_HOUSE_2 ? '𓅢' : null}
-					{i === SAFE_HOUSE_3 ? '𐦝' : null}
-					{space}
-				</div>
-			))}
+		<div className="flex flex-col gap-2">
+			{Array.from({ length: NUM_ROWS }).map((_, rowNum) => {
+				const indexStart = rowNum * NUM_COLUMNS;
+				const indexEnd = (rowNum + 1) * NUM_COLUMNS;
+
+				return (
+					<div
+						className={clsx('flex gap-2', rowNum === 1 && 'flex-row-reverse')}
+						key={rowNum}
+					>
+						{spaces.slice(indexStart, indexEnd).map((item, i) => (
+							<Space item={item} index={indexStart + i} key={i} />
+						))}
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
+interface SpaceProps {
+	item: Item;
+	index: number;
+}
+
+function Space({ item, index }: SpaceProps) {
+	return (
+		<div className="w-24 aspect-square grid place-items-center border-2 text-5xl cursor-pointer select-none">
+			{index === HOUSE_OF_REBIRTH ? '☥' : null}
+			{index === SAFE_HOUSE_1 ? '𓆾' : null}
+			{index === HOUSE_OF_WATER ? '𓈗' : null}
+			{index === SAFE_HOUSE_2 ? '𓅢' : null}
+			{index === SAFE_HOUSE_3 ? '𐦝' : null}
+			{item}
 		</div>
 	);
 }
