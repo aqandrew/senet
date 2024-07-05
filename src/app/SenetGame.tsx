bcf3eb4c38332d9c165ba6ba38f85eeb4a383267
Andrew Aquino <dawneraq@gmail.com>
2024-07-05 14:00:10 -0700

    feat: add sections incl sticks

--- src/app/SenetGame.tsx
+++ src/app/SenetGame.tsx
@@ -16,31 +16,78 @@ const HOUSE_OF_WATER = 26;
 const SAFE_HOUSE_2 = 27;
 const SAFE_HOUSE_3 = 28;
 
-export default function SenetBoard() {
+type Stick = 0 | 1 | null;
+
+export default function SenetGame() {
 	const [spaces, setSpaces] = useState<Item[]>(
 		new Array(30)
 			.fill(null)
 			.map((_, i) => (i < 10 ? (i % 2 ? BLACK_PAWN : WHITE_PAWN) : null))
 	);
+	const [turnNum, setTurnNum] = useState(0);
+	const [sticks, setSticks] = useState<Stick[]>([null, null, null, null]);
+
+	// TODO account for extra turns
+	const squaresToMove = sticks.reduce(
+		(total: number, stick) => total + stick!,
+		0
+	);
+
+	// fill sticks with random bits
+	function rollSticks() {
+		setSticks(sticks.map((_) => Math.round(Math.random()) as Stick));
+	}
 
 	return (
-		<div className="flex flex-col gap-2">
-			{Array.from({ length: NUM_ROWS }).map((_, rowNum) => {
-				const indexStart = rowNum * NUM_COLUMNS;
-				const indexEnd = (rowNum + 1) * NUM_COLUMNS;
+		<>
+			<section className="mb-6">
+				<h2 className="font-bold text-xl mb-2">Game board</h2>
 
-				return (
-					<div
-						className={clsx('flex gap-2', rowNum === 1 && 'flex-row-reverse')}
-						key={rowNum}
-					>
-						{spaces.slice(indexStart, indexEnd).map((item, i) => (
-							<Space item={item} index={indexStart + i} key={i} />
-						))}
-					</div>
-				);
-			})}
-		</div>
+				<div className="flex flex-col gap-2">
+					{Array.from({ length: NUM_ROWS }).map((_, rowNum) => {
+						const indexStart = rowNum * NUM_COLUMNS;
+						const indexEnd = (rowNum + 1) * NUM_COLUMNS;
+
+						return (
+							<div
+								className={clsx(
+									'flex gap-2',
+									rowNum === 1 && 'flex-row-reverse'
+								)}
+								key={rowNum}
+							>
+								{spaces.slice(indexStart, indexEnd).map((item, i) => (
+									<Space item={item} index={indexStart + i} key={i} />
+								))}
+							</div>
+						);
+					})}
+				</div>
+			</section>
+
+			<section className="mb-6">
+				<h2 className="font-bold text-xl mb-2">
+					Sticks:{' '}
+					{sticks.every((stick) => stick === null)
+						? 'not yet rolled'
+						: squaresToMove}
+				</h2>
+				{/* TODO graphical representation of sticks */}
+				<button
+					className="rounded p-2 bg-gray-200 cursor-pointer"
+					onClick={rollSticks}
+				>
+					Roll sticks
+				</button>
+			</section>
+
+			<section className="mb-6">
+				<h2 className="font-bold text-xl mb-2">Game status</h2>
+
+				<p>turn number: {turnNum}</p>
+				<p>{turnNum % 2 ? 'white' : 'black'}'s turn</p>
+			</section>
+		</>
 	);
 }
 
